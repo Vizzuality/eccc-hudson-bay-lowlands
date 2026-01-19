5b167a8455a122af92798667d4f3a246c78f4a4b
atrincas <atrincas@gmail.com>
2026-01-19 10:57:46 +0100

    initial setup frontend client

---
+++ client/src/app/[locale]/layout.tsx
@@ -0,0 +1,45 @@
+import { Sofia_Sans, Tinos } from "next/font/google";
+import { notFound } from "next/navigation";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
+import { setRequestLocale } from "next-intl/server";
+import { routing } from "@/i18n/routing";
+import "@/app/globals.css";
+
+const sofiaSans = Sofia_Sans({
+	variable: "--font-sofia-sans",
+	subsets: ["latin"],
+});
+
+const tinos = Tinos({
+	variable: "--font-tinos",
+	subsets: ["latin"],
+	weight: ["400", "700"],
+});
+
+export function generateStaticParams() {
+	return routing.locales.map((locale) => ({ locale }));
+}
+
+export default async function LocaleLayout({
+	children,
+	params,
+}: Readonly<{
+	children: React.ReactNode;
+	params: Promise<{ locale: string }>;
+}>) {
+	// Ensure that the incoming `locale` is valid
+	const { locale } = await params;
+	if (!hasLocale(routing.locales, locale)) {
+		notFound();
+	}
+
+	setRequestLocale(locale);
+
+	return (
+		<html lang={locale}>
+			<body className={`${sofiaSans.variable} ${tinos.variable} antialiased`}>
+				<NextIntlClientProvider>{children}</NextIntlClientProvider>
+			</body>
+		</html>
+	);
+}
