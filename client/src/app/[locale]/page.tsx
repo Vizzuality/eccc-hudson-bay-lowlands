6ccf05ff342b0b422383d51aee7045b8d352c5b4
atrincas <atrincas@gmail.com>
2026-02-15 17:07:26 +0100

    fix lint errors

--- client/src/app/[locale]/page.tsx
+++ client/src/app/[locale]/page.tsx
@@ -4,27 +4,27 @@ import MapSidebar from "@/containers/map-sidebar";
 import TopBar from "@/containers/top-bar";
 
 export async function generateMetadata({
-	params,
+  params,
 }: {
-	params: Promise<{ locale: string }>;
+  params: Promise<{ locale: string }>;
 }) {
-	const { locale } = await params;
-	const t = await getTranslations({ locale, namespace: "metadata.home" });
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "metadata.home" });
 
-	return {
-		title: t("title"),
-		description: t("description"),
-	};
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
 }
 
 export default function Home() {
-	return (
-		<main className="h-screen flex flex-col">
-			<TopBar />
-			<section className="flex h-full">
-				<MapSidebar />
-				<MapContainer className="flex-1 rounded-tl-3xl"></MapContainer>
-			</section>
-		</main>
-	);
+  return (
+    <main className="h-screen flex flex-col">
+      <TopBar />
+      <section className="flex h-full">
+        <MapSidebar />
+        <MapContainer className="flex-1 rounded-tl-3xl"></MapContainer>
+      </section>
+    </main>
+  );
 }
