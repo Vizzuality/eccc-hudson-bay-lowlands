7231290b16896e5f44448bcc9f93d5810c053339
atrincas <atrincas@gmail.com>
2026-02-15 17:46:46 +0100

    add placeholder test for analyze data flow

---
+++ e2e/tests/home/analyze-map.spec.ts
@@ -0,0 +1,27 @@
+import { expect, test } from "@playwright/test";
+
+test("Page has correct title", async ({ page }) => {
+	await page.goto("http://localhost:3000");
+
+	await expect(page).toHaveTitle(/Discover the Hudson & James bay region/);
+});
+
+test.describe("Analyze", () => {
+	test("User can analyze data by uploading a file", async ({ page }) => {
+		await page.goto("http://localhost:3000");
+
+		await page.getByRole("button", { name: "Analyze area" }).click();
+
+		await page.getByRole("button", { name: "Upload" }).click();
+
+		await page.getByRole("button", { name: "Confirm" }).click();
+
+		await expect(page).toHaveURL(
+			"http://localhost:3000/en?mapStatus=analysis&mapShape=true",
+		);
+
+		await expect(
+			page.getByRole("heading", { name: "My area of interest" }),
+		).toBeVisible();
+	});
+});
