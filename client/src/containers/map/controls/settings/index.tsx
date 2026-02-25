68b8c99fc775f6df3e13108bf09de2fec6312bfb
atrincas <atrincas@gmail.com>
2026-02-25 17:56:50 +0100

    update styles map controls

--- client/src/containers/map/controls/settings/index.tsx
+++ client/src/containers/map/controls/settings/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { TooltipPortal } from "@radix-ui/react-tooltip";
 import { SettingsIcon } from "lucide-react";
 import {
   type FC,
@@ -13,11 +12,6 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 import { CONTROL_BUTTON_STYLES } from "../constants";
 
@@ -36,41 +30,29 @@ export const SettingsControl: FC<PropsWithChildren<SettingsControlProps>> = ({
   return (
     <div className={cn("flex flex-col space-y-0.5", className)}>
       <Popover onOpenChange={setPopoverOpen}>
-        <Tooltip delayDuration={300}>
-          <PopoverTrigger asChild>
-            <TooltipTrigger asChild autoFocus={false}>
-              <button
-                id={id}
-                className={cn({
-                  [CONTROL_BUTTON_STYLES.default]: true,
-                  [CONTROL_BUTTON_STYLES.hover]: true,
-                  [CONTROL_BUTTON_STYLES.active]: true,
-                  [CONTROL_BUTTON_STYLES.open]: popoverOpen,
-                })}
-                aria-label="Map settings"
-                type="button"
-              >
-                <SettingsIcon />
-              </button>
-            </TooltipTrigger>
-          </PopoverTrigger>
-
-          {!popoverOpen && (
-            <TooltipPortal>
-              <TooltipContent side="left" align="center">
-                <div className="text-xxs">Map settings</div>
-              </TooltipContent>
-            </TooltipPortal>
-          )}
-
-          <PopoverContent
-            side="left"
-            align="start"
-            className="w-auto overflow-hidden p-6"
+        <PopoverTrigger asChild>
+          <button
+            id={id}
+            className={cn({
+              [CONTROL_BUTTON_STYLES.default]: true,
+              [CONTROL_BUTTON_STYLES.hover]: true,
+              [CONTROL_BUTTON_STYLES.active]: true,
+              [CONTROL_BUTTON_STYLES.open]: popoverOpen,
+            })}
+            aria-label="Map settings"
+            type="button"
           >
-            {children}
-          </PopoverContent>
-        </Tooltip>
+            <SettingsIcon className={CONTROL_BUTTON_STYLES.icon} />
+          </button>
+        </PopoverTrigger>
+
+        <PopoverContent
+          side="left"
+          align="start"
+          className="w-auto overflow-hidden p-6"
+        >
+          {children}
+        </PopoverContent>
       </Popover>
     </div>
   );
