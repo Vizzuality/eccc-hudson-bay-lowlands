57ee6aa2f061584046cccbf01cabcd5d9f019155
alexeh <alexurola@gmail.com>
2026-03-12 08:07:42 +0100

    chore(infra): spin up staging

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -79,6 +79,41 @@ module "dev" {
   # }
 }
 
+module "staging" {
+  source      = "./modules/env"
+  project     = var.project_name
+  environment = "staging"
+  domain      = "staging.eccc.dev-vizzuality.com"
+
+  aws_region = var.aws_region
+
+  vpc                = module.vpc.vpc
+  subnet_ids         = module.vpc.public_subnet_ids
+  availability_zones = module.vpc.availability_zones
+
+  beanstalk_platform = "64bit Amazon Linux 2023 v4.9.1 running Docker"
+  beanstalk_tier     = "WebServer"
+  ec2_instance_type  = "t3.medium"
+
+  elasticbeanstalk_iam_service_linked_role_name = "AWSServiceRoleForElasticBeanstalk"
+  cname_prefix                                  = "${var.project_name}-staging-environment"
+
+  rds_instance_class = "db.t3.micro"
+  repo_name          = var.repo_name
+  github_owner       = var.github_owner
+  github_token       = var.github_token
+
+  # TODO: Enable when basic auth is implemented in the application
+  # basic_auth_enabled  = var.basic_auth_enabled
+  # basic_auth_user     = var.basic_auth_user
+  # basic_auth_password = var.basic_auth_password
+
+  # TODO: Enable when GitHub integration module is implemented
+  # github_additional_environment_variables = {
+  #   JWT_EXPIRES_IN = "1d"
+  # }
+}
+
 
 
 module "github" {
