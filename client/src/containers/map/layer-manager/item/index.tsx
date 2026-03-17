ee98595e8b1b885eb1f3b2c26c2c66f4ad49b3a6
alexeh <alexurola@gmail.com>
2026-03-17 23:51:44 +0100

    fix: sort imports

--- client/src/containers/map/layer-manager/item/index.tsx
+++ client/src/containers/map/layer-manager/item/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 import { useSyncLayersSettings } from "@/app/[locale]/url-store";
 import RasterLayerManagerItem from "@/containers/map/layer-manager/item/raster-item";
 import VectorLayerManagerItem from "@/containers/map/layer-manager/item/vector-item";
