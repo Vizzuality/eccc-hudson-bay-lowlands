1d274d029b65fda160bf3dca42bf79463542859b
atrincas <atrincas@gmail.com>
2026-03-27 12:37:14 +0100

    initial setup treeMap

--- client/src/containers/widgets/ecosystem-types.tsx
+++ client/src/containers/widgets/ecosystem-types.tsx
@@ -2,12 +2,58 @@ import { GlobeIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
 import type { FC } from "react";
 import RichText from "@/components/ui/rich-text";
+import TreeMap from "@/containers/charts/tree-map";
 import MoreInfoTooltip from "@/containers/more-info-tooltip";
 import { WidgetCard, WidgetCardIcon } from "@/containers/widgets/card";
 import type { WidgetCardBaseProps } from "@/containers/widgets/types";
+import { useApiTranslation } from "@/i18n/api-translation";
 
+const mockData = [
+  {
+    key: "eco_temperate_perc",
+    label: { en: "Temperate", fr: "Tempéré" },
+    value: 50,
+  },
+  {
+    key: "eco_treed_perc",
+    label: { en: "Treed", fr: "Arboré" },
+    value: 30,
+  },
+  {
+    key: "eco_grassland_perc",
+    label: { en: "Grassland", fr: "Prairie" },
+    value: 20,
+  },
+  {
+    key: "eco_graminoid_perc",
+    label: { en: "Graminoid", fr: "Graminoid" },
+    value: 10,
+  },
+];
+const chartConfig = {
+  eco_temperate_perc: {
+    color: "var(--color-green-800)",
+  },
+  eco_treed_perc: {
+    color: "var(--color-green-600)",
+  },
+  eco_grassland_perc: {
+    color: "var(--color-green-300)",
+  },
+  eco_graminoid_perc: {
+    color: "var(--color-green-800)",
+  },
+};
 const EcosystemTypes: FC<WidgetCardBaseProps> = ({ id }) => {
   const t = useTranslations("widgets.ecosystem-types");
+  const { getTranslation } = useApiTranslation();
+  const data = mockData.map((item) => ({
+    key: item.key,
+    label: getTranslation(item.label),
+    value: item.value,
+    fill: chartConfig[item.key as keyof typeof chartConfig].color,
+  }));
+
   return (
     <WidgetCard
       id={id}
@@ -37,6 +83,7 @@ const EcosystemTypes: FC<WidgetCardBaseProps> = ({ id }) => {
       <MoreInfoTooltip title={t("more-info.title")}>
         {t("more-info.description")}
       </MoreInfoTooltip>
+      <TreeMap data={data} chartConfig={chartConfig} />
     </WidgetCard>
   );
 };
