442b49a062def87adb87352322ab7b37e22291e7
atrincas <atrincas@gmail.com>
2026-04-23 11:42:13 +0200

    style(e2e): fix biome formatting (tabs → spaces)

--- e2e/tests/home/navigation.spec.ts
+++ e2e/tests/home/navigation.spec.ts
@@ -1,28 +1,30 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("Navigation", () => {
-	test("page loads with the correct title", async ({ page }) => {
-		await page.goto("http://localhost:3000");
+  test("page loads with the correct title", async ({ page }) => {
+    await page.goto("http://localhost:3000");
 
-		await expect(page).toHaveTitle(/Discover the Hudson & James bay region/);
-	});
+    await expect(page).toHaveTitle(/Discover the Hudson & James bay region/);
+  });
 
-	test("switching to French updates the URL locale", async ({ page }) => {
-		await page.goto("http://localhost:3000/en");
+  test("switching to French updates the URL locale", async ({ page }) => {
+    await page.goto("http://localhost:3000/en");
 
-		await page.getByRole("combobox").click();
-		await page.getByRole("option", { name: "French" }).click();
+    await page.getByRole("combobox").click();
+    await page.getByRole("option", { name: "French" }).click();
 
-		await expect(page).toHaveURL(/\/fr/);
-	});
+    await expect(page).toHaveURL(/\/fr/);
+  });
 
-	// TODO: Failing tests, times out
-	test.skip("switching back to English updates the URL locale", async ({ page }) => {
-		await page.goto("http://localhost:3000/fr");
+  // TODO: Failing tests, times out
+  test.skip("switching back to English updates the URL locale", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:3000/fr");
 
-		await page.getByRole("combobox").click();
-		await page.getByRole("option", { name: "English" }).click();
+    await page.getByRole("combobox").click();
+    await page.getByRole("option", { name: "English" }).click();
 
-		await expect(page).toHaveURL(/\/en/);
-	});
+    await expect(page).toHaveURL(/\/en/);
+  });
 });
