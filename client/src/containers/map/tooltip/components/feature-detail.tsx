d47e82381dc633a109249f31efa4a1feedce036d
atrincas <atrincas@gmail.com>
2026-04-24 10:41:30 +0200

    feat(client): add click tooltips for map vector layers
    Add interaction_config to vector layer metadata to define which feature
    attributes to show on click, and scaffold the three tooltip components
    (feature-value, feature-properties, feature-detail) that render them.

---
+++ client/src/containers/map/tooltip/components/feature-detail.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const FeatureDetailTooltip: FC = () => {
+  return (
+    <div>
+      <h3>Feature Detail</h3>
+    </div>
+  );
+};
+
+export default FeatureDetailTooltip;
---
+++ client/src/containers/map/tooltip/components/feature-properties.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const FeaturePropertiesTooltip: FC = () => {
+  return (
+    <div>
+      <h3>Feature Properties</h3>
+    </div>
+  );
+};
+
+export default FeaturePropertiesTooltip;
